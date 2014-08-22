06c14d6b3396a5f8036aba8e31e84a3e16facc30
MattMer <mgmershon@gmail.com>
2014-08-21 19:33:05 -0700

    C# Version of Project 5 (Vending Machine)
    This is a simple vending machine created with C#. It is a test project
    to practice switching over Java experience to C#.

---
+++ CSPractice/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace CSPractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Instructions = new System.Windows.Forms.GroupBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.Instructions.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cola";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Yellow;
+            this.button2.Location = new System.Drawing.Point(6, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Lemon-Lime";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button3.Location = new System.Drawing.Point(6, 131);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 50);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Grape";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button4.Location = new System.Drawing.Point(6, 187);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 50);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Root Beer";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button5.Location = new System.Drawing.Point(6, 243);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 50);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Bottled Water";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Location = new System.Drawing.Point(157, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(89, 304);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Beverages";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 2;
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 104);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Currency";
+            // 
+            // Instructions
+            // 
+            this.Instructions.Controls.Add(this.label2);
+            this.Instructions.Location = new System.Drawing.Point(12, 143);
+            this.Instructions.Name = "Instructions";
+            this.Instructions.Size = new System.Drawing.Size(120, 115);
+            this.Instructions.TabIndex = 9;
+            this.Instructions.TabStop = false;
+            this.Instructions.Text = "Instructions";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 16);
+            this.label2.MaximumSize = new System.Drawing.Size(100, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 90);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "There are only 5 units of each drink. Each drink costs $0.75";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 338);
+            this.Controls.Add(this.Instructions);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Vending Machine";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.Instructions.ResumeLayout(false);
+            this.Instructions.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox Instructions;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CSPractice/Form1.cs
@@ -0,0 +1,107 @@
+﻿// Matthew G. Mershon
+// Vending Machine program
+//C# , MVS2012
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSPractice
+{
+    public partial class Form1 : Form
+    {
+        int colaTotal = 0;
+        int lemonTotal = 0;
+        int grapeTotal = 0;  //These are initializing the click count for the drinks.
+        int rootTotal = 0;
+        int waterTotal = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void checkAndCharge() //Method used for each drink button. 
+        {
+            decimal money = numericUpDown1.Value; //Pulls value from user input.
+            double doubleMoney = decimal.ToDouble(money); //Converts it to double.
+            if (doubleMoney < .75) //Each drink is $0.75.
+            {
+                MessageBox.Show("You have insufficient funds.", "Sorry"); //When the transaction is unsuccessful.
+            }
+            else
+            {
+                doubleMoney = doubleMoney - .75; //Subtract the soda cost from user input.
+                money = Convert.ToDecimal(doubleMoney); //Returns to Decimal to replace into field.
+                numericUpDown1.Value = money; //Setting field to new total.
+                MessageBox.Show("Enjoy your beverage!", "Enjoy!"); //When the transaction is successful.
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Cola button.
+        {
+            if (colaTotal > 5)
+            {
+                MessageBox.Show("There are no more drinks of this type!", "Sorry");
+            }
+            else
+            checkAndCharge();
+            colaTotal++;
+        }
+
+        private void button2_Click(object sender, EventArgs e) //Lemon button.
+        {
+            if (lemonTotal > 5)
+            {
+                MessageBox.Show("There are no more drinks of this type!", "Sorry");
+            }
+            else
+            checkAndCharge();
+            lemonTotal++;
+        }
+
+        private void button3_Click(object sender, EventArgs e) //Grape button.
+        {
+            if (grapeTotal > 5)
+            {
+                MessageBox.Show("There are no more drinks of this type!", "Sorry");
+            }
+            else
+            checkAndCharge();
+            grapeTotal++;
+        }
+
+        private void button4_Click(object sender, EventArgs e) //Rootbeer button.
+        {
+            if (rootTotal > 5)
+            {
+                MessageBox.Show("There are no more drinks of this type!", "Sorry");
+            }
+            else
+            checkAndCharge();
+            rootTotal++;
+        }
+
+        private void button5_Click(object sender, EventArgs e) //Water bottle button.
+        {
+            if (waterTotal > 5)
+            {
+                MessageBox.Show("There are no more drinks of this type!", "Sorry");
+            }
+            else
+            checkAndCharge();
+            waterTotal++;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e) //Field for money.
+        {
+
+        }
+    }
+}
